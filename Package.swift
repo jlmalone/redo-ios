b1385dc97abba8b6b2a76d5130ac82723c1725af
Joseph Malone <malone.joseph@gmail.com>
2025-11-09 15:58:59 -0500

    Add missing dependencies to Package.swift
    - Added GoogleSignIn-iOS package dependency
    - Added GoogleSignIn product to RedoUI target
    - Added RedoWidgets and RedoIntents targets for iOS-specific features
    - All dependencies now properly configured for build

--- Package.swift
+++ Package.swift
@@ -19,10 +19,19 @@ let package = Package(
         .library(
             name: "RedoUI",
             targets: ["RedoUI"]
+        ),
+        .library(
+            name: "RedoWidgets",
+            targets: ["RedoWidgets"]
+        ),
+        .library(
+            name: "RedoIntents",
+            targets: ["RedoIntents"]
         )
     ],
     dependencies: [
         .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.0.0"),
+        .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "7.0.0"),
         .package(url: "https://github.com/apple/swift-crypto", from: "3.0.0")
     ],
     targets: [
@@ -48,7 +57,24 @@ let package = Package(
             dependencies: [
                 "RedoCore",
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk")
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS")
+            ]
+        ),
+
+        // Widgets (iOS-exclusive)
+        .target(
+            name: "RedoWidgets",
+            dependencies: [
+                "RedoCore"
+            ]
+        ),
+
+        // Siri Shortcuts (iOS-exclusive)
+        .target(
+            name: "RedoIntents",
+            dependencies: [
+                "RedoCore"
             ]
         ),
 
